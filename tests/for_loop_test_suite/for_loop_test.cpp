9ff8b3ea03eead741eb630d803baa2630b296a45
nathanwrightUNR <nathanwright@nevada.unr.edu>
2024-10-22 23:54:16 +0000

    better for loop test

--- tests/for_loop_test_suite/for_loop_test.cpp
+++ tests/for_loop_test_suite/for_loop_test.cpp
@@ -9,13 +9,17 @@
 
 TEST(ForLoopTest, ForLoopIncrement)
 {
-    auto code = "for var x: int = 0; x <= 5; x += 1 do print x;";
+    auto code = "var z = 0"
+                "for var x: int = 0; x <= 5; x += 1 do"
+                "z = x;"
+                "print z;";
+
     auto ast = parse_code(code);
 
     Interpreter interpreter;
     interpreter.evaluate(&ast);
 
-    ASSERT_TRUE(interpreter.environment->contains("x"));
-    ASSERT_TRUE(is_type<int>(interpreter.environment->get("x")));
-    EXPECT_EQ(as_type<int>(interpreter.environment->get("x").data), 5);
+    ASSERT_TRUE(interpreter.environment->contains("z"));
+    ASSERT_TRUE(is_type<int>(interpreter.environment->get("z")));
+    EXPECT_EQ(as_type<int>(interpreter.environment->get("z").data), 5);
 }
