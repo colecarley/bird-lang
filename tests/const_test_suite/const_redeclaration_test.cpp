87a0abae2679270905859f7332532bec60514724
Cole Carley <cole.carley@gmail.com>
2024-12-04 04:17:40 +0000

    added bird testing workflow

--- tests/const_test_suite/const_redeclaration_test.cpp
+++ tests/const_test_suite/const_redeclaration_test.cpp
@@ -1,11 +1,11 @@
 #include <gtest/gtest.h>
-#include "../helpers/compile_helper.hpp"
+#include "helpers/compile_helper.hpp"
 
 TEST(ConstTest, ConstRedeclaration)
 {
     BirdTest::TestOptions options;
-    options.code = "const x = 0;"
-                   "const x = 1;";
+    options.code = "var x = 0;"
+                   "var x = 1;";
     options.type_check = false;
     options.interpret = false;
     options.compile = false;
@@ -20,4 +20,31 @@ TEST(ConstTest, ConstRedeclaration)
     };
 
     ASSERT_FALSE(BirdTest::compile(options));
+
+    options.after_semantic_analyze = [&](UserErrorTracker &error_tracker, SemanticAnalyzer &analyze_semantics)
+    {
+        ASSERT_TRUE(error_tracker.has_errors());
+    };
+
+    BirdTest::compile(options);
+}
+
+TEST(ConstTest, Foobar)
+{
+    BirdTest::TestOptions options;
+    options.code = "var x = 0; print x; print x + 1;";
+
+    options.after_interpret = [&](Interpreter &interpreter)
+    {
+        ASSERT_TRUE(interpreter.env.contains("x"));
+        ASSERT_TRUE(is_type<int>(interpreter.env.get("x")));
+        ASSERT_EQ(as_type<int>(interpreter.env.get("x")), 0);
+    };
+
+    options.after_compile = [&](std::string &output)
+    {
+        ASSERT_EQ(output, "0\n1\n\n");
+    };
+
+    BirdTest::compile(options);
 }
